e2d1efb423d0b11e96d4e53564d8e77d89aed532
Ryan Lima <138833109+Ryan-Felix-Lima@users.noreply.github.com>
2023-08-05 17:03:52 -0300

    Add files via upload

---
+++ js/script2.js
@@ -0,0 +1,8 @@
+document.write("<h1> Executando script2.js</h1>")
+document.write("Esse script esta em um arquivo externo ")
+document.write("Referencia no fim do body do HTML")
+console.log("Primeiro")
+console.log('Segundo')
+var teste =1
+teste ='João'
+document.write('O valor da variavel é '+teste)
